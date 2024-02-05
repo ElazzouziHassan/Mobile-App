7fa4b8c3195819b6b60879220884b10fa53f6afb
wizardy <wizardhassan99@gmail.com>
2024-02-05 16:56:35 +0100

    commit

--- app/src/main/java/com/example/my_app/MainActivity.java
+++ app/src/main/java/com/example/my_app/MainActivity.java
@@ -3,18 +3,7 @@ package com.example.my_app;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.widget.TextView;
 
-import com.android.volley.RequestQueue;
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
-import com.android.volley.toolbox.JsonObjectRequest;
-import com.android.volley.toolbox.Volley;
-
-import org.json.JSONException;
-import org.json.JSONObject;
-
-import java.lang.ref.ReferenceQueue;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -23,31 +12,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        TextView tv = (TextView) findViewById(R.id.textview);
+
 
         String url = "https://jsonplaceholder.typicode.com/todos/1";
 
-        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(url, null, new Response.Listener<JSONObject>() {
-            @Override
-            public void onResponse(JSONObject response) {
-                try {
-                    int userId = response.getInt("userId");
-                    int id = response.getInt("id");
-                    String title = response.getString("title");
-                    boolean status = response.getBoolean("completed");
-                    tv.setText(userId+"\n"+id+"\n"+title+"\n"+status);
-                } catch (JSONException e){
-                    e.printStackTrace();
-                }
-            }
-        }, new Response.ErrorListener() {
-            @Override
-            public void onErrorResponse(VolleyError error) {
-                tv.setText("error ");
-            }
-        });
 
-        RequestQueue requestQueue = Volley.newRequestQueue(this);
-        requestQueue.add(jsonObjectRequest);
     }
 }
