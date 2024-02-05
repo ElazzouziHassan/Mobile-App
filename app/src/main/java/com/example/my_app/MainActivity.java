5abf6bd01b98de8c5e426ff2a8b5fe260e12cf6d
wizardy <wizardhassan99@gmail.com>
2024-02-05 17:14:45 +0100

    commit

--- app/src/main/java/com/example/my_app/MainActivity.java
+++ app/src/main/java/com/example/my_app/MainActivity.java
@@ -3,19 +3,53 @@ package com.example.my_app;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONObject;
 
 
 public class MainActivity extends AppCompatActivity {
+    private TextView resTextViewResult;
+    private RequestQueue reqQueue;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        resTextViewResult = findViewById(R.id.text_view_result);
+        Button btnSubmit = findViewById(R.id.btn_submit);
+        reqQueue = Volley.newRequestQueue(this);
+        btnSubmit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                JsonParse();
+            }
+        });
 
-
+    }
+    private void JsonParse() {
         String url = "https://jsonplaceholder.typicode.com/todos/1";
 
+        JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
 
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                error.printStackTrace();
+            }
+        });
     }
 }
